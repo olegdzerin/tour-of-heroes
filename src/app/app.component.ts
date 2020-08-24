b54bc85bb15c0d096ad4a6ed80693933a380324b
olegdzerin <oleg.dz2171@gmail.com>
2020-08-24 16:27:30 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tour-of-herous';
+export class AppComponent implements OnInit {
+title = "Herous";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
